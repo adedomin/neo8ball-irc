8648b2aafb96db4ea5051a040e7f3b5e1777bc3b
adedomin <anthony@dedominic.pw>
2016-11-12 17:41:44 -0500

    fixed decide, invalid else if

--- lib/decide.sh
+++ lib/decide.sh
@@ -3,6 +3,6 @@ reg="(.*) or (.*)\?" # decide
 reg2="(.*)\?" # regular 8ball msg
 if [[ "$4" =~ $reg ]]; then
     echo ":m $1 $3: ${BASH_REMATCH[($RANDOM % 2)+1]}"
-else if [[ "$4" =~ $reg2 ]]; then
+elif [[ "$4" =~ $reg2 ]]; then
     ./lib/8ball.sh "$1" "$2" "$3" "$4"
 fi
