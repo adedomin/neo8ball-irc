acb5487826a76196f8a6809eb4e4e4df56159e87
mike <dimattiami>
2016-12-30 16:16:26 -0500

    Send message to channel

--- lib/define.sh
+++ lib/define.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- echo "http://www.dictionary.com/browse/$1" |
+RES=$(echo "http://www.dictionary.com/browse/$1" |
  wget -O- -i- --quiet | 
  hxnormalize -x 2>/dev/null | 
  hxselect -i "div.def-set" 2>/dev/null |  
@@ -8,4 +8,9 @@
  xargs 2>/dev/null |
  sed $'s/1\./\\\n1./g' |
  awk -F '1.|4.' '{print $2}' |
- tail -n +2
+ tail -n +2)
+
+IFS=$'\n' 
+for res in $RES; do
+    echo ":m $1 $res"
+done
