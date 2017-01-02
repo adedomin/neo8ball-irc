f786d16244b9da5c8af347c4ef63a42b1a6fbfa6
adedomin <anthony@dedominic.pw>
2017-01-01 19:25:37 -0500

    fixed search

--- lib/vidme.sh
+++ lib/vidme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-URI='https://vid.me/api/videos/search?query='
-RES=$(curl "${URI}${4}" 2>/dev/null | \
+URI='https://vid.me/api/videos/search?order=hot&query='
+RES=$(curl "${URI}$(sed 's/ /+/g' <<< "$4")" 2>/dev/null | \
     jq -r '.videos[0],.videos[1],.videos[2] | .full_url + " :: " + .title'
 )
 
