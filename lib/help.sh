181be1c5bb19b9c6d462f495e8424fb802bd0ba1
adedomin <anthony@dedominic.pw>
2017-01-07 18:01:02 -0500

    updated help

--- lib/help.sh
+++ lib/help.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo -e ":m $1 \0028ball\002 .8ball <question>? :: \002Web Search\002 .ddg <query> :: \002Weather\002 .w <location> :: \002Set Location\002 .location <location> :: \002Vidme\002 .v <query>"
+echo -e ":m $1 \0028ball\002 .8ball <question>? :: \002Web Search\002 .ddg <query> :: \002Weather\002 .w <location> :: \002Set Location\002 .location <location> :: \002Vidme\002 .v <query> :: \002NWS METAR OBV\002 .nws <station> :: \002Set Station\002 .nwsd <station>"
