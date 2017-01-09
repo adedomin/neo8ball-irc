2126d5434a83b97d9644164c11ccc054185059ea
adedomin <anthony@dedominic.pw>
2017-01-09 02:40:41 -0500

    fixed define, added license ty so much dimattia

--- lib/define.sh
+++ lib/define.sh
@@ -1,19 +1,34 @@
-#!/usr/bin/env bash
-# not impl
-exit 0
+#!/bin/bash
+# Copyright 2017 dimattiami, prussian <genunrest@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-URI='http://www.dictionary.com/browse/'
+URI_ENCODE() {
+    curl -Gso /dev/null \
+        -w '%{url_effective}' \
+        --data-urlencode @- '' <<< "$1" | \
+    cut -c 3-
+}
 
-RES=$(curl "${URI}${4}" 2>/dev/null | \
- hxnormalize -x 2>/dev/null | \
- hxselect -i "div.def-set" 2>/dev/null | \
- lynx -stdin -dump 2>/dev/null | \
- sed $'s/1\./\\\n1./g' | \
- awk -F '1.|4.' '{print $2}' | \
- tail -n +2
-)
-
-IFS=$'\n' 
-for res in $RES; do
-    echo ":m $1 $res"
+echo "http://www.dictionary.com/browse/$(URI_ENCODE "$4")" |
+    wget -O- -i- --quiet | 
+    hxnormalize -x 2>/dev/null | 
+    hxselect -i "div.def-set" 2>/dev/null |  
+    lynx -stdin -dump 2>/dev/null |
+    xargs 2>/dev/null |
+    sed 's/[0-9]\./\n&/g' |
+    head -n 4 |
+    sed '/^$/d' |
+while read -r definition; do
+    echo ":m $1 $definition"
 done
