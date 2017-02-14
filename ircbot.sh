67dd07307ec52a808adf863f556b2dbd7fb394ce
adedomin <anthony@dedominic.pw>
2017-02-13 22:17:22 -0500

    version incr

--- ircbot.sh
+++ ircbot.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VERSION="bash-ircbot: v1.4.0-Beta"
+VERSION="bash-ircbot: v1.4.1-Beta"
 
 usage() {
     echo "usage: $0 [-c config]"
