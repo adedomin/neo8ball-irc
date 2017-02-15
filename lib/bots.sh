93055dfa4c21bbb9cfea3e4e9fb922547ed567a6
adedomin <anthony@dedominic.pw>
2017-02-14 21:26:41 -0500

    formatting, new help tag etc

--- lib/bots.sh
+++ lib/bots.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-echo -e ":m $1 \002neo8ball\002 [bash] :: \002Source\002 https://github.com/GeneralUnRest/neo8ball-irc"
+echo -e ":m $1 \002neo8ball\002 [bash] ::" \
+    "\002Source\002 https://github.com/GeneralUnRest/neo8ball-irc"
--- lib/youtube.sh
+++ lib/youtube.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+[ -z "$4" ] && exit 0
+
 URI_ENCODE() {
     curl -Gso /dev/null \
         -w '%{url_effective}' \
