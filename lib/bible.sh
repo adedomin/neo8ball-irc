306d84f21926735d97f092011d723194384b4284
adedomin <anthony@dedominic.pw>
2017-05-10 22:46:29 -0400

    generic nothing found text if empty

--- lib/bible.sh
+++ lib/bible.sh
@@ -32,7 +32,7 @@ if [ -n "$BIBLE_DB" ] && [ -f "$BIBLE_DB" ]; then
         q="\"$q\""
     fi
 
-    printf ":m $1 %s\n" "$(sqlite3 "$BIBLE_DB" << EOF
+    verse="$(sqlite3 "$BIBLE_DB" << EOF
 SELECT * FROM $table 
 WHERE $table 
 MATCH '$q' 
@@ -40,14 +40,21 @@ ORDER BY rank
 LIMIT 1;
 EOF
     )"
+    
+    echo ":m $1 ${verse:-Nothing Found}"
+
 elif [ -n "$BIBLE_SOURCE" ] && [ -f "$BIBLE_SOURCE" ]; then
     
     if [ -z "$4" ]; then
-        printf ":m $1 %s\n" "$(shuf -n1 "$BIBLE_SOURCE")"
+        echo ":m $1 $(shuf -n1 "$BIBLE_SOURCE")"
         exit 0
     fi
 
-    printf ":m $1 %s\n" "$(grep -F -m 1 "$4" "$BIBLE_SOURCE")"
+    verse="$(grep -F -m 1 "$4" "$BIBLE_SOURCE")"
+    echo ":m $1 ${verse:-Nothing Found}"
+
 else
+
     echo ":m $1 No bible available"
+
 fi
