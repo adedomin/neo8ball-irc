7e83afa9bcb48aaac11b2a66149a0a8409e4eb65
adedomin <anthony@dedominic.pw>
2017-05-21 16:23:23 -0400

    point moose to my moose

--- lib/moose.sh
+++ lib/moose.sh
@@ -54,28 +54,28 @@ top_border() {
 
 KCOL=$'\003'
 COLOR=(
-''
-$'\003'"00,00"
-$'\003'"01,01"
-$'\003'"02,02"
-$'\003'"03,03"
-$'\003'"04,04"
-$'\003'"05,05"
-$'\003'"06,06"
-$'\003'"07,07"
-$'\003'"08,08"
-$'\003'"09,09"
-$'\003'"10,10"
-$'\003'"11,11"
-$'\003'"12,12"
-$'\003'"13,13"
-$'\003'"14,14"
-$'\003'"15,15"
+['t']=''
+['0']=$'\003'"00,00"
+['1']=$'\003'"01,01"
+['2']=$'\003'"02,02"
+['3']=$'\003'"03,03"
+['4']=$'\003'"04,04"
+['5']=$'\003'"05,05"
+['6']=$'\003'"06,06"
+['7']=$'\003'"07,07"
+['8']=$'\003'"08,08"
+['9']=$'\003'"09,09"
+['a']=$'\003'"10,10"
+['b']=$'\003'"11,11"
+['c']=$'\003'"12,12"
+['d']=$'\003'"13,13"
+['e']=$'\003'"14,14"
+['f']=$'\003'"15,15"
 )
 
 # get moose and moose meta data
 MOOSE="$(
-    curl "http://captmoose.club/view/$(URI_ENCODE "${4:-random}")" 2>/dev/null
+    curl "http://ghetty.space:7512/moose/$(URI_ENCODE "${4:-random}")" 2>/dev/null
 )"
 MOOSE_ERR="$(jq -r '.error' <<< "$MOOSE")"
 # check for error
@@ -90,17 +90,14 @@ MOOSE_IMAGE=()
 while read -r line; do
     MOOSE_IMAGE+=("$line")
 done < <( 
-    # a special retard apparently thinks a string that
-    # represents a valid json array is somehow clever or something
-    jq -r '.image' <<< "$MOOSE" \
-        | jq -r '.[] | map(tostring) | join(" ")' # fucking STUPID
+    jq -r '.image' <<< "$MOOSE"
 )
 
 # trim moose image
 #check from top down
 for (( i=0; i<${#MOOSE_IMAGE[@]}; i++ )); do
     line="${MOOSE_IMAGE[$i]}"
-    line="${line//0}"
+    line="${line//t}"
     line="${line// }"
     if [ -z "$line" ]; then
         unset MOOSE_IMAGE["$i"]
@@ -116,7 +113,7 @@ MOOSE_IMAGE=("${MOOSE_IMAGE[@]}")
 #check from down up
 for (( i=${#MOOSE_IMAGE[@]}; i>=0; i-- )); do
     line="${MOOSE_IMAGE[$i]}"
-    line="${line//0}"
+    line="${line//t}"
     line="${line// }"
     if [ -z "$line" ]; then
         unset MOOSE_IMAGE["$i"]
@@ -131,16 +128,16 @@ MOOSE_IMAGE=("${MOOSE_IMAGE[@]}")
 # trim from left to right
 for (( i=0; i<${#MOOSE_IMAGE}; i++ )); do
     for (( j=0; j<${#MOOSE_IMAGE[@]}; j++ )); do
-        read -r first other <<< "${MOOSE_IMAGE[$j]}"
-        if [ "$first" != '0' ]; then 
+        line="${MOOSE_IMAGE[$j]}"
+        if [ "${line:0:1}" != 't' ]; then 
             noleft=1
             break
         fi
     done
     [ -n "$noleft" ] && break
     for (( j=0; j<${#MOOSE_IMAGE[@]}; j++ )); do
-        read -r first other <<< "${MOOSE_IMAGE[$j]}"
-        MOOSE_IMAGE[$j]="$other"
+        line="${MOOSE_IMAGE[$j]}"
+        MOOSE_IMAGE[$j]="${line:1}"
     done
 done
 unset noleft
@@ -148,28 +145,26 @@ unset noleft
 # trim from right to left
 for (( i=0; i<${#MOOSE_IMAGE}; i++ )); do
     for (( j=0; j<${#MOOSE_IMAGE[@]}; j++ )); do
-        read -r -a elements <<< "${MOOSE_IMAGE[$j]}"
-        if [ "${elements[-1]}" != '0' ]; then 
+        line="${MOOSE_IMAGE[$j]}"
+        if [ "${line: -1}" != 't' ]; then 
             noleft=1
             break
         fi
     done
     [ -n "$noleft" ] && break
     for (( j=0; j<${#MOOSE_IMAGE[@]}; j++ )); do
-        read -r -a elements <<< "${MOOSE_IMAGE[$j]}"
-        unset elements[-1]
-        MOOSE_IMAGE[$j]="${elements[*]}"
+        line="${MOOSE_IMAGE[$j]}"
+        MOOSE_IMAGE[$j]="${line:0:-1}"
     done
 done
 
 for line in "${MOOSE_IMAGE[@]}"; do
     out=''
-    line=($line)
-    for (( i=0; i<${#line[@]}; i++ )); do
-        if [ "${line[$i]}" = '0' ]; then
+    for (( i=0; i<${#line}; i++ )); do
+        if [ "${line:$i:1}" = 't' ]; then
             out+=' '
         else
-            out+="${COLOR[${line[$i]}]}@${KCOL}"
+            out+="${COLOR[${line:$i:1}]}@${KCOL}"
         fi
     done
     echo ":m $1 |$out"
