21727b2787214e1eacedb6e7877b2851c0ef00f8
Anthony DeDominic <adedomin@gmail.com>
2020-07-25 15:25:34 -0400

    use date for version

--- ircbot.sh
+++ ircbot.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VERSION="neo8ball: v6.0.2"
+VERSION="neo8ball: v2020.07.25"
 
 echo1() {
     printf '%s\n' "$*"
