60fb7fceab7a3ce50a1baf03f18f9b146a383fee
Anthony DeDominic <adedomin@gmail.com>
2020-08-11 16:01:41 -0400

    increment version

--- ircbot.sh
+++ ircbot.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VERSION="neo8ball: v2020.07.28"
+VERSION="neo8ball: v2020.8.11"
 
 echo1() {
     printf '%s\n' "$*"
